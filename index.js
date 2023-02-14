127db8d447dd015038acb8f5a0fbaa55cb4766c9
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-02-14 18:49:09 +0200

    Fix Linter error

--- index.js
+++ index.js
@@ -3,65 +3,63 @@ const container = document.querySelector('.big-lists-container');
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 
-class BookStorage {
-  constructor(title, author, id) {
-    this.title = title;
-    this.author = author;
-    this.id = id;
-  }
-}
-
-class Library {
-  constructor() {
-    this.books = JSON.parse(localStorage.getItem('awesomeBooks')) || [];
-    this.displaydata();
-    this.remove();
-  }
-  static cleanInputs(){
-    title.value='';
-    author.value='';
+class AwesomeLibrary {
+  static cleanInputs() {
+    title.value = '';
+    author.value = '';
   }
 
-  addBook(title, author) {
-    const id = this.books.length + 1;
-    const book = new BookStorage(title, author, id);
-    this.books.push(book);
-    localStorage.setItem('awesomeBooks', JSON.stringify(this.books));
-    this.displaydata();
+  static addBook(title, author) {
+    const books = JSON.parse(localStorage.getItem('awesomeBooks')) || [];
+    const id = books.length + 1;
+    const book = { title, author, id };
+    books.push(book);
+    localStorage.setItem('awesomeBooks', JSON.stringify(books));
+    this.displaydata(books);
   }
 
-  removeBook(id) {
-    this.books = this.books.filter(book => book.id !== id);
-    localStorage.setItem('awesomeBooks', JSON.stringify(this.books));
-    this.displaydata();
+  static removeBook(id) {
+    const books = JSON.parse(localStorage.getItem('awesomeBooks')) || [];
+    const updatedBooks = books.filter((book) => book.id !== id);
+    localStorage.setItem('awesomeBooks', JSON.stringify(updatedBooks));
+    this.displaydata(updatedBooks);
   }
 
-  displaydata() {
-    const uniqueBooks = this.books.filter((book, index, arr) => arr.findIndex((t) => t.id === book.id) === index);
-    const datas = uniqueBooks.map((book) => {
-      return `<div class="book-lists">
+  static displaydata(books) {
+    const uniqueBooks = books.filter(
+      (book, index, arr) => arr.findIndex((t) => t.id === book.id) === index,
+    );
+    const datas = uniqueBooks
+      .map(
+        (book) => `<div class="book-lists">
                 <div><p>"${book.title}"</p>by<p>${book.author}</p></div>
                 <button class="clean" data-id=${book.id}>Remove</button>
-              </div>`;
-    }).join('');
+              </div>`,
+      )
+      .join('');
     container.innerHTML = datas;
   }
 
-  remove() {
+  static remove() {
     container.addEventListener('click', (e) => {
       if (e.target.className.includes('clean')) {
         const dataId = e.target.dataset.id;
-        this.removeBook(parseInt(dataId));
+        this.removeBook(parseInt(dataId, 10));
       }
     });
   }
+
+  static initialize() {
+    const books = JSON.parse(localStorage.getItem('awesomeBooks')) || [];
+    this.displaydata(books);
+    this.remove();
+  }
 }
 
-const library = new Library();
+AwesomeLibrary.initialize();
 
 formButton.addEventListener('submit', (e) => {
   e.preventDefault();
-  library.addBook(title.value, author.value);
-  Library.cleanInputs();
-  
+  AwesomeLibrary.addBook(title.value, author.value);
+  AwesomeLibrary.cleanInputs();
 });
